e360fec05115d09ffb72935a08f726c1340181e3
Liz <lizphil88@gmail.com>
2019-07-30 07:56:01 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -42,25 +42,25 @@ function switchAge (age){
 var age = 15;
  switch (age) {
   case 13:
-   console.log("You are a teenager");
+   return ("You are a teenager");
   break;
   case 14:
-  console.log("You are a teenager");
+  return ("You are a teenager");
   break;
   case 15:
-  console.log("You are a teenager");
+  return ("You are a teenager");
   break;
   case 16:
-  console.log("You are a teenager");
+  return ("You are a teenager");
   break;
   case 17:
-  console.log("You are a teenager");
+  return ("You are a teenager");
   break;
   case 18:
-  console.log("You are a teenager");
+  return ("You are a teenager");
   break;
   case 19:
-  console.log("You are a teenager");
+  return ("You are a teenager");
   break;
   default:
   return ("You have an age");
